3f5fc2914f40e4f3b0e62ec93db3aaf9d4f1e792
Fabian Norman <fabiann@gmail.com>
2012-11-24 22:52:59 -0800

    Cleanout the test lines.

--- etc_editor.sh
+++ etc_editor.sh
@@ -22,39 +22,26 @@ i=1
 n=0
 declare -a difffiles
 
-echo "Test-i is ${i}, and n is ${n}" #Test Line
-
 directory=${1%/*}
 file=${1##*/}
-
-echo "Test-dir is ${directory} and file is ${file}" #Test Line
 
 mkdir /tmp/etc_keeper
 cp $1 /tmp/etc_keeper/$file
 
 $EDITOR $1
 
-echo "Test-editor end"
-
 until [[ $i == 0 ]]; do
     diffname="${1}.diff$[n+1]"
-    echo "Test-looking for: $diffname" #Test Line
     if [ -e $diffname ]; then
-        echo "Test-FOUND IT!" #Test Line
         difffiles[$n]=$diffname # Build the array
-        echo "Test-added to array entry #$n." #Test Line
         n=$[$n+1]
     else
-        echo "Test-DIDN'T FIND IT!" #Test Line
         difffiles[$n]=$diffname # This will be the new diff file
-        echo "Test-End, added #$n" #Test Line
         i=0
     fi
 done
 
 diff -u /tmp/etc_keeper/$file $1 > "${difffiles[-1]}"
-
-echo "Test-create a file at ${difffiles[-1]}" #Test Line
 
 ##
 # Cleanup files
